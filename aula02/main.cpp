d94cb6cbffebba389622b79f65d85d75467ca5dc
Artur Santiago <artursantiago2001@hotmail.com>
2020-06-16 10:47:41 -0300

    aula02

---
+++ aula02/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+  std::string message = "";
+  std::string add = "add";
+
+  if (argc == 1 || (argc >= 2 && add.compare(argv[1]) != 0)) {
+    std::cout << "CORRECT USE: " << argv[0] << " add <message> \nOR: " << argv[0] << " add \n";
+    return 1;
+  }
+
+  if (argc > 2) {
+    message.append(argv[2]);
+    std::cout << "The message \"" << argv[2] << "\" was sucessfully stored!\n";
+  } else {
+    std::cout << "Type the message to be stored: ";
+    getline(std::cin, message);
+    std::cout << "The message was sucessfully stored!\n";
+  }
+  
+  return 0;
+}
