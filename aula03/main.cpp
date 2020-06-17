17a8ca22e624cec4552ef55dd4207da5b382b9d1
Artur Santiago <artursantiago2001@hotmail.com>
2020-06-17 09:29:44 -0300

    aula03

--- aula03/main.cpp
+++ aula03/main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -5,19 +6,24 @@ int main(int argc, char const *argv[])
 {
   std::string message = "";
 
-  if (argc == 1 || (argc >= 2 && std::string(argv[1]).compare("add") != 0)) {
+  std::ofstream output_file{"output.txt"};
+
+  if (argc == 1 || (argc >= 2 && std::string(argv[1]) != "add")) {
     std::cout << "CORRECT USE: " << argv[0] << " add <message> \nOR: " << argv[0] << " add \n";
     return 1;
   }
-
+  
   if (argc > 2) {
-    message.append(argv[2]);
-    std::cout << "The message \"" << argv[2] << "\" was sucessfully stored!\n";
+    message = argv[2];
   } else {
     std::cout << "Type the message to be stored: ";
     getline(std::cin, message);
-    std::cout << "The message was successfully stored!\n";
   }
   
+  output_file << message;
+  output_file.close();
+
+  std::cout << "The message was sucessfully stored!\n";
+
   return 0;
 }
